7c4e5462f1a69b139d643bde554c064a4a5c3423
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-08-13 09:37:06 +0700

    :sparkles: Add register/table creation

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql2');
+const crypto = require('crypto');
+const dbExec = require('./db-exec'); 
+
+const config = require('./config/serverConfig');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser({ extended: true }));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  database: 'swDevLab',
+  user: 'root',
+  password: 'admin1234',
+  multipleStatements: true
+});
+
+db.connect((err) => {
+  if (!err) {
+    console.log("Connected to database!");
+    db.execute(dbExec.createTable, (err, res, fields) => {
+      if (err) console.log(err.message);
+    });
+  } else {
+    console.log("Error connecting database");
+  }
+});
 
 app.get('/', (req, res) => {
   res.send("Hello world!");
 });
 
+app.post('/login', (req, res) => {
+  res.send(req.body);
+});
+
+app.post('/register', (req, res) => {
+  let hash = crypto.createHash('md5').update(req.body.password).digest("hex");
+  db.query(
+    'INSERT INTO users (fullname, username, password) VALUES (?,?,?)',
+    [req.body.fullname, req.body.username, hash],
+    (err, results) => {
+      if (err) res.send({ error: err });
+      res.send({ "message": "Registered successful!" });
+    }
+  );
+});
+
 app.listen(5000, (req, res) => {
-  console.log("Listening on port 3000");
+  console.log("🚀 Listening on port 5000");
 });
---
+++ backend/db-exec.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+const config = require('../backend/config/serverConfig');
+
+module.exports.createTable = `
+  CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    fullname VARCHAR(20),
+    username VARCHAR(30),
+    password CHAR(32), 
+    signup_date TIMESTAMP NOT NULL DEFAULT NOW()
+  );
+`;
